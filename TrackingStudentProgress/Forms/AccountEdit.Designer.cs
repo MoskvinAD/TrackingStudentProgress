d64cea135219fe43817e89948d8ef287aa839dc1
Андрей <admoskvin99@bk.ru>
2023-06-05 20:29:33 +0400

    Не много поправил внешний вид

--- TrackingStudentProgress/Forms/AccountEdit.Designer.cs
+++ TrackingStudentProgress/Forms/AccountEdit.Designer.cs
@@ -47,6 +47,8 @@
             this.label8 = new System.Windows.Forms.Label();
             this.addbt = new System.Windows.Forms.Button();
             this.deletebt = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // comboBoxAccount
@@ -54,71 +56,71 @@
             this.comboBoxAccount.FormattingEnabled = true;
             this.comboBoxAccount.Location = new System.Drawing.Point(12, 28);
             this.comboBoxAccount.Name = "comboBoxAccount";
-            this.comboBoxAccount.Size = new System.Drawing.Size(315, 21);
+            this.comboBoxAccount.Size = new System.Drawing.Size(326, 21);
             this.comboBoxAccount.TabIndex = 0;
             this.comboBoxAccount.SelectedIndexChanged += new System.EventHandler(this.comboBoxAccount_SelectedIndexChanged);
             // 
             // classcbox
             // 
             this.classcbox.FormattingEnabled = true;
-            this.classcbox.Location = new System.Drawing.Point(67, 155);
+            this.classcbox.Location = new System.Drawing.Point(173, 73);
             this.classcbox.Name = "classcbox";
-            this.classcbox.Size = new System.Drawing.Size(121, 21);
+            this.classcbox.Size = new System.Drawing.Size(78, 21);
             this.classcbox.TabIndex = 11;
             this.classcbox.SelectedIndexChanged += new System.EventHandler(this.classcbox_SelectedIndexChanged);
             // 
             // FirstName
             // 
-            this.FirstName.Location = new System.Drawing.Point(13, 116);
+            this.FirstName.Location = new System.Drawing.Point(11, 72);
             this.FirstName.Name = "FirstName";
-            this.FirstName.Size = new System.Drawing.Size(121, 20);
+            this.FirstName.Size = new System.Drawing.Size(156, 20);
             this.FirstName.TabIndex = 12;
             this.FirstName.TextChanged += new System.EventHandler(this.FirstName_TextChanged);
             // 
             // MidlName
             // 
-            this.MidlName.Location = new System.Drawing.Point(140, 116);
+            this.MidlName.Location = new System.Drawing.Point(11, 114);
             this.MidlName.Name = "MidlName";
-            this.MidlName.Size = new System.Drawing.Size(121, 20);
+            this.MidlName.Size = new System.Drawing.Size(156, 20);
             this.MidlName.TabIndex = 13;
             this.MidlName.TextChanged += new System.EventHandler(this.MidlName_TextChanged);
             // 
             // LastName
             // 
-            this.LastName.Location = new System.Drawing.Point(267, 116);
+            this.LastName.Location = new System.Drawing.Point(13, 154);
             this.LastName.Name = "LastName";
-            this.LastName.Size = new System.Drawing.Size(121, 20);
+            this.LastName.Size = new System.Drawing.Size(156, 20);
             this.LastName.TabIndex = 14;
             this.LastName.TextChanged += new System.EventHandler(this.LastName_TextChanged);
             // 
             // Login
             // 
-            this.Login.Location = new System.Drawing.Point(67, 77);
+            this.Login.Location = new System.Drawing.Point(10, 32);
             this.Login.Name = "Login";
-            this.Login.Size = new System.Drawing.Size(122, 20);
+            this.Login.Size = new System.Drawing.Size(157, 20);
             this.Login.TabIndex = 15;
             this.Login.TextChanged += new System.EventHandler(this.Login_TextChanged);
             // 
             // Password
             // 
-            this.Password.Location = new System.Drawing.Point(195, 76);
+            this.Password.Location = new System.Drawing.Point(173, 32);
             this.Password.Name = "Password";
-            this.Password.Size = new System.Drawing.Size(121, 20);
+            this.Password.Size = new System.Drawing.Size(147, 20);
             this.Password.TabIndex = 16;
             this.Password.TextChanged += new System.EventHandler(this.Password_TextChanged);
             // 
             // Position
             // 
             this.Position.FormattingEnabled = true;
-            this.Position.Location = new System.Drawing.Point(194, 155);
+            this.Position.Location = new System.Drawing.Point(173, 115);
             this.Position.Name = "Position";
-            this.Position.Size = new System.Drawing.Size(121, 21);
+            this.Position.Size = new System.Drawing.Size(147, 21);
             this.Position.TabIndex = 17;
             this.Position.SelectedIndexChanged += new System.EventHandler(this.Position_SelectedIndexChanged);
             // 
             // editbt
             // 
-            this.editbt.Location = new System.Drawing.Point(333, 7);
+            this.editbt.Location = new System.Drawing.Point(358, 12);
             this.editbt.Name = "editbt";
             this.editbt.Size = new System.Drawing.Size(85, 28);
             this.editbt.TabIndex = 18;
@@ -138,7 +140,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(46, 99);
+            this.label2.Location = new System.Drawing.Point(8, 55);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 20;
@@ -147,7 +149,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(182, 99);
+            this.label3.Location = new System.Drawing.Point(8, 97);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(29, 13);
             this.label3.TabIndex = 21;
@@ -156,7 +158,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(305, 99);
+            this.label4.Location = new System.Drawing.Point(10, 137);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(54, 13);
             this.label4.TabIndex = 22;
@@ -165,7 +167,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(64, 61);
+            this.label5.Location = new System.Drawing.Point(7, 16);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(38, 13);
             this.label5.TabIndex = 23;
@@ -174,7 +176,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(192, 60);
+            this.label6.Location = new System.Drawing.Point(170, 16);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(45, 13);
             this.label6.TabIndex = 24;
@@ -183,7 +185,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(64, 139);
+            this.label7.Location = new System.Drawing.Point(170, 57);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(38, 13);
             this.label7.TabIndex = 25;
@@ -192,7 +194,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(191, 139);
+            this.label8.Location = new System.Drawing.Point(170, 99);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(65, 13);
             this.label8.TabIndex = 26;
@@ -200,7 +202,7 @@
             // 
             // addbt
             // 
-            this.addbt.Location = new System.Drawing.Point(333, 38);
+            this.addbt.Location = new System.Drawing.Point(358, 46);
             this.addbt.Name = "addbt";
             this.addbt.Size = new System.Drawing.Size(85, 28);
             this.addbt.TabIndex = 27;
@@ -210,7 +212,7 @@
             // 
             // deletebt
             // 
-            this.deletebt.Location = new System.Drawing.Point(333, 69);
+            this.deletebt.Location = new System.Drawing.Point(358, 79);
             this.deletebt.Name = "deletebt";
             this.deletebt.Size = new System.Drawing.Size(85, 28);
             this.deletebt.TabIndex = 28;
@@ -218,32 +220,44 @@
             this.deletebt.UseVisualStyleBackColor = true;
             this.deletebt.Click += new System.EventHandler(this.deletebt_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Password);
+            this.groupBox1.Controls.Add(this.Login);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.LastName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.classcbox);
+            this.groupBox1.Controls.Add(this.MidlName);
+            this.groupBox1.Controls.Add(this.Position);
+            this.groupBox1.Controls.Add(this.FirstName);
+            this.groupBox1.Location = new System.Drawing.Point(12, 55);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(326, 191);
+            this.groupBox1.TabIndex = 29;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Данные";
+            // 
             // AccountEdit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(430, 197);
+            this.ClientSize = new System.Drawing.Size(455, 260);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.deletebt);
             this.Controls.Add(this.addbt);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.editbt);
-            this.Controls.Add(this.Position);
-            this.Controls.Add(this.Password);
-            this.Controls.Add(this.Login);
-            this.Controls.Add(this.LastName);
-            this.Controls.Add(this.MidlName);
-            this.Controls.Add(this.FirstName);
-            this.Controls.Add(this.classcbox);
             this.Controls.Add(this.comboBoxAccount);
             this.Name = "AccountEdit";
             this.ShowIcon = false;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -270,5 +284,6 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Button addbt;
         private System.Windows.Forms.Button deletebt;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
--- TrackingStudentProgress/Forms/StudentEdit.Designer.cs
+++ TrackingStudentProgress/Forms/StudentEdit.Designer.cs
@@ -50,12 +50,14 @@
             this.editbt = new System.Windows.Forms.Button();
             this.label8 = new System.Windows.Forms.Label();
             this.classbox = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label14
             // 
             this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(270, 95);
+            this.label14.Location = new System.Drawing.Point(150, 99);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(65, 13);
             this.label14.TabIndex = 78;
@@ -63,16 +65,16 @@
             // 
             // Telegram
             // 
-            this.Telegram.Location = new System.Drawing.Point(273, 111);
+            this.Telegram.Location = new System.Drawing.Point(153, 115);
             this.Telegram.Name = "Telegram";
-            this.Telegram.Size = new System.Drawing.Size(100, 20);
+            this.Telegram.Size = new System.Drawing.Size(139, 20);
             this.Telegram.TabIndex = 77;
             this.Telegram.TextChanged += new System.EventHandler(this.Telegram_TextChanged);
             // 
             // label16
             // 
             this.label16.AutoSize = true;
-            this.label16.Location = new System.Drawing.Point(154, 95);
+            this.label16.Location = new System.Drawing.Point(150, 60);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(37, 13);
             this.label16.TabIndex = 76;
@@ -80,15 +82,15 @@
             // 
             // Email
             // 
-            this.Email.Location = new System.Drawing.Point(157, 111);
+            this.Email.Location = new System.Drawing.Point(153, 76);
             this.Email.Name = "Email";
-            this.Email.Size = new System.Drawing.Size(100, 20);
+            this.Email.Size = new System.Drawing.Size(139, 20);
             this.Email.TabIndex = 75;
             this.Email.TextChanged += new System.EventHandler(this.Email_TextChanged);
             // 
             // DateBirth
             // 
-            this.DateBirth.Location = new System.Drawing.Point(12, 111);
+            this.DateBirth.Location = new System.Drawing.Point(153, 34);
             this.DateBirth.Name = "DateBirth";
             this.DateBirth.Size = new System.Drawing.Size(139, 20);
             this.DateBirth.TabIndex = 74;
@@ -97,7 +99,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(9, 94);
+            this.label6.Location = new System.Drawing.Point(150, 17);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(86, 13);
             this.label6.TabIndex = 65;
@@ -106,7 +108,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(270, 49);
+            this.label4.Location = new System.Drawing.Point(3, 99);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(54, 13);
             this.label4.TabIndex = 64;
@@ -115,7 +117,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(146, 49);
+            this.label3.Location = new System.Drawing.Point(3, 60);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(29, 13);
             this.label3.TabIndex = 63;
@@ -124,7 +126,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 49);
+            this.label2.Location = new System.Drawing.Point(3, 18);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 62;
@@ -132,23 +134,23 @@
             // 
             // OStudent
             // 
-            this.OStudent.Location = new System.Drawing.Point(273, 68);
+            this.OStudent.Location = new System.Drawing.Point(6, 115);
             this.OStudent.Name = "OStudent";
-            this.OStudent.Size = new System.Drawing.Size(108, 20);
+            this.OStudent.Size = new System.Drawing.Size(131, 20);
             this.OStudent.TabIndex = 57;
             this.OStudent.TextChanged += new System.EventHandler(this.OStudent_TextChanged);
             // 
             // NStudent
             // 
-            this.NStudent.Location = new System.Drawing.Point(149, 68);
+            this.NStudent.Location = new System.Drawing.Point(6, 76);
             this.NStudent.Name = "NStudent";
-            this.NStudent.Size = new System.Drawing.Size(118, 20);
+            this.NStudent.Size = new System.Drawing.Size(131, 20);
             this.NStudent.TabIndex = 56;
             this.NStudent.TextChanged += new System.EventHandler(this.NStudent_TextChanged);
             // 
             // FStudent
             // 
-            this.FStudent.Location = new System.Drawing.Point(12, 68);
+            this.FStudent.Location = new System.Drawing.Point(6, 37);
             this.FStudent.Name = "FStudent";
             this.FStudent.Size = new System.Drawing.Size(131, 20);
             this.FStudent.TabIndex = 55;
@@ -157,7 +159,7 @@
             // Mazerbox
             // 
             this.Mazerbox.FormattingEnabled = true;
-            this.Mazerbox.Location = new System.Drawing.Point(157, 156);
+            this.Mazerbox.Location = new System.Drawing.Point(153, 158);
             this.Mazerbox.Name = "Mazerbox";
             this.Mazerbox.Size = new System.Drawing.Size(139, 21);
             this.Mazerbox.TabIndex = 79;
@@ -168,14 +170,14 @@
             this.studentbox.FormattingEnabled = true;
             this.studentbox.Location = new System.Drawing.Point(12, 25);
             this.studentbox.Name = "studentbox";
-            this.studentbox.Size = new System.Drawing.Size(226, 21);
+            this.studentbox.Size = new System.Drawing.Size(232, 21);
             this.studentbox.TabIndex = 80;
             this.studentbox.SelectedIndexChanged += new System.EventHandler(this.studentbox_SelectedIndexChanged);
             // 
             // FazerBox
             // 
             this.FazerBox.FormattingEnabled = true;
-            this.FazerBox.Location = new System.Drawing.Point(12, 156);
+            this.FazerBox.Location = new System.Drawing.Point(8, 158);
             this.FazerBox.Name = "FazerBox";
             this.FazerBox.Size = new System.Drawing.Size(139, 21);
             this.FazerBox.TabIndex = 81;
@@ -193,7 +195,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 140);
+            this.label5.Location = new System.Drawing.Point(8, 142);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(32, 13);
             this.label5.TabIndex = 83;
@@ -202,7 +204,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(158, 140);
+            this.label7.Location = new System.Drawing.Point(154, 142);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(33, 13);
             this.label7.TabIndex = 84;
@@ -210,9 +212,9 @@
             // 
             // addbt
             // 
-            this.addbt.Location = new System.Drawing.Point(318, 18);
+            this.addbt.Location = new System.Drawing.Point(328, 18);
             this.addbt.Name = "addbt";
-            this.addbt.Size = new System.Drawing.Size(55, 28);
+            this.addbt.Size = new System.Drawing.Size(70, 29);
             this.addbt.TabIndex = 86;
             this.addbt.Text = "Добавить";
             this.addbt.UseVisualStyleBackColor = true;
@@ -222,7 +224,7 @@
             // 
             this.editbt.Location = new System.Drawing.Point(250, 18);
             this.editbt.Name = "editbt";
-            this.editbt.Size = new System.Drawing.Size(57, 28);
+            this.editbt.Size = new System.Drawing.Size(72, 29);
             this.editbt.TabIndex = 85;
             this.editbt.Text = "Изменить";
             this.editbt.UseVisualStyleBackColor = true;
@@ -231,7 +233,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(310, 140);
+            this.label8.Location = new System.Drawing.Point(303, 17);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(38, 13);
             this.label8.TabIndex = 88;
@@ -240,41 +242,53 @@
             // classbox
             // 
             this.classbox.FormattingEnabled = true;
-            this.classbox.Location = new System.Drawing.Point(313, 156);
+            this.classbox.Location = new System.Drawing.Point(306, 33);
             this.classbox.Name = "classbox";
             this.classbox.Size = new System.Drawing.Size(73, 21);
             this.classbox.TabIndex = 87;
             this.classbox.SelectedIndexChanged += new System.EventHandler(this.classbox_SelectedIndexChanged);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.FStudent);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.classbox);
+            this.groupBox1.Controls.Add(this.NStudent);
+            this.groupBox1.Controls.Add(this.label14);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.Telegram);
+            this.groupBox1.Controls.Add(this.OStudent);
+            this.groupBox1.Controls.Add(this.label16);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.Email);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.DateBirth);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.Mazerbox);
+            this.groupBox1.Controls.Add(this.FazerBox);
+            this.groupBox1.Location = new System.Drawing.Point(13, 53);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(385, 196);
+            this.groupBox1.TabIndex = 89;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Данные";
+            // 
             // StudentEdit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(393, 206);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.classbox);
+            this.ClientSize = new System.Drawing.Size(404, 263);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.addbt);
             this.Controls.Add(this.editbt);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label5);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.FazerBox);
             this.Controls.Add(this.studentbox);
-            this.Controls.Add(this.Mazerbox);
-            this.Controls.Add(this.label14);
-            this.Controls.Add(this.Telegram);
-            this.Controls.Add(this.label16);
-            this.Controls.Add(this.Email);
-            this.Controls.Add(this.DateBirth);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.OStudent);
-            this.Controls.Add(this.NStudent);
-            this.Controls.Add(this.FStudent);
             this.Name = "StudentEdit";
             this.ShowIcon = false;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -304,5 +318,6 @@
         private System.Windows.Forms.Button editbt;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.ComboBox classbox;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
